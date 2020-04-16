a7b734de6f5805e3a69ada260f6a458d5e113f95
Hehaonan <stackoverflow163@163.com>
2020-04-16 18:17:13 +0800

    add:BST中查找某值

--- javalib/src/main/java/com/android/hhn/javalib/binarytree/BinarySearchTreePractice.java
+++ javalib/src/main/java/com/android/hhn/javalib/binarytree/BinarySearchTreePractice.java
@@ -84,22 +84,48 @@ public class BinarySearchTreePractice {
      * @return
      */
     private static boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 排除只有一个节点的情况
+        if (root.left == null && root.right == null) {
+            return false;
+        }
         return isValidBST(root, null, null);
     }
 
     private static boolean isValidBST(TreeNode root, TreeNode minNode, TreeNode maxNode) {
-        if (null == root) {
+        if (root == null) {
             return true;
         }
         //针对左子树 如果大于节点的值 BTS不成立
-        if (null != maxNode && root.value >= maxNode.value)
+        if (maxNode != null && root.value >= maxNode.value)
             return false;
         //针对右子树 如果小于节点的值 BTS不成立
-        if (null != minNode && root.value <= minNode.value)
+        if (minNode != null && root.value <= minNode.value)
             return false;
-        // 左子树：当前root->maxNode
-        // 右子树：当前root->minNode
+        // 左子树：当前节点为最大节点
+        // 右子树：当前节点为最小节点
         return isValidBST(root.left, minNode, root) && isValidBST(root.right, root, maxNode);
+    }
+
+    /**
+     * 判断BST中是否存在某值
+     *
+     * @param root
+     * @param target
+     *
+     * @return
+     */
+    private static boolean isInBST(TreeNode root, int target) {
+        if (null == root) {
+            return false;
+        }
+        if (root.value == target)
+            return true;
+        if (target > root.value)
+            return isInBST(root.right, target);
+        return isInBST(root.left, target);
     }
 
     public static void main(String[] args) {
@@ -116,7 +142,10 @@ public class BinarySearchTreePractice {
         TreeNode b = new TreeNode(d, e, 8);
         TreeNode root = new TreeNode(b, c, 10);
 
-        System.out.println("是否是BTS：" + isValidBST(root));
+        //System.out.println("是否是BTS：" + isValidBST(root));
+
+        int target = 14;
+        System.out.println(target + "是否在BTS：" + isInBST(root, target));
     }
 
 }
