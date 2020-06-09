6b2a98d97e04eec589541a49796685aba1b1ce62
Hehaonan <stackoverflow163@163.com>
2020-06-09 20:13:51 +0800

    fix:自定义priority queue Comparator实现大小顶堆

--- javalib/src/main/java/com/android/hhn/javalib/queue/PriorityQueuePractice.java
+++ javalib/src/main/java/com/android/hhn/javalib/queue/PriorityQueuePractice.java
@@ -1,5 +1,7 @@
 package com.android.hhn.javalib.queue;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -19,19 +21,37 @@ public class PriorityQueuePractice {
 
     private static int getKthLargest(int[] array, int k) {
         // java默认是小顶堆
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k, getComparator());
         for (int i : array) {
             if (priorityQueue.size() < k) {
                 priorityQueue.offer(i);
             } else {
-                if (priorityQueue.peek() < i) { // 每个元素和对顶比较
+                // 默认小顶堆 可以找出最大值
+                if (priorityQueue.peek() < i) { // 如果是大顶堆 queue.peek > i，入栈规则不一样
                     priorityQueue.poll();//弹出栈顶最小的
                     priorityQueue.offer(i);
                 }
             }
         }
+        System.out.println(Arrays.toString(priorityQueue.toArray()));
         return priorityQueue.peek() == null ? -1 : priorityQueue.peek();
     }
 
+    /**
+     * 自定义 Comparator 接口
+     *
+     * @return
+     */
+    private static Comparator<Integer> getComparator() {
+        // 默认小顶堆 找最大值 升序排列 o1-o2
+        // 大顶堆 找最小值 降序排列 o2-o1
+        Comparator<Integer> comparator = new Comparator<Integer>() { // lambda表达式 (o1, o2) -> o1 - o2;
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        };
+        return comparator;
+    }
 
 }
